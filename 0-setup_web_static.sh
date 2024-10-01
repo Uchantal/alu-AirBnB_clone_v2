db37c677705893363b2ca7a063d677e9ab8af45e
Uchantal <156025831+Uchantal@users.noreply.github.com>
2024-10-01 19:53:21 +0200

    configured

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,32 +1,38 @@
 #!/usr/bin/env bash
-# describe it then
+# Sets up a web server for deployment of web_static.
+
 apt-get update
-apt-get -y install nginx
+apt-get install -y nginx
 
-directories=("/data/web_static/releases/test" "/data/web_static/shared/")
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-for directory in "${directories[@]}"; do
-  #  if [ ! -e "$directory" ]; then
-  mkdir -p "$directory"
-  #  fi
-done
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
 
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" >/data/web_static/releases/test/index.html
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
 
-# Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder.
-# If the symbolic link already exists, it should be deleted and recreated every time the script is ran
-ln --symbolic --force /data/web_static/releases/test /data/web_static/current
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
 
-# The -R option ensures that the ownership changes are applied
-# recursively to all files and directories within the folder
-chown -R ubuntu:ubuntu /data/
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
 
-sed -i '/listen 80 default_server;/a \ \n    location /hbnb_static {\n        alias /data/web_static/current/;\n        index index.html;\n    }' /etc/nginx/sites-available/default
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
 
 service nginx restart
